bac168eb00611badaacfd4c961193de2a092141d
NabeelahY <noblenab13@gmail.com>
2020-07-30 10:35:10 +0100

    Group posts according to subreddits

--- src/view/Home.tsx
+++ src/view/Home.tsx
@@ -12,21 +12,37 @@ import {
 import Nav from '../components/Navbar';
 import { getPosts } from '../state/actions/posts';
 import PostCard from '../components/PostCard';
+import { Post } from '../state/actions/posts.types';
 
 type Props = ReturnType<typeof mapStateToProps> & any;
 const Home: React.FC<Props> = ({ getPosts, posts }) => {
+  const [val, setVal] = useState('');
+
   useEffect(() => {
     getPosts();
   }, [getPosts]);
+
+  const filteredPosts = posts.filter((post: Post) => {
+    return post.data.title.toLowerCase().includes(val.toLowerCase());
+  });
+  const groupedPosts = filteredPosts.reduce((acc: any, curr: any) => {
+    (acc[curr.data.subreddit] = acc[curr.data.subreddit] || []).push(curr);
+    return acc;
+  }, {});
+  console.log(groupedPosts);
+
   return (
     <>
-      <Nav />
-      <Container className="section">
-        {posts
-          ? posts.map((post: any, idx: number) => (
+      <Nav val={val} setVal={setVal} />
+      <Container>
+        {Object.entries(groupedPosts).map(([group, posts]: any, idx: any) => (
+          <>
+            <h1>{group.toUpperCase()}</h1>
+            {posts.map((post: any, idx: any) => (
               <PostCard post={post} key={idx} />
-            ))
-          : null}
+            ))}
+          </>
+        ))}
       </Container>
     </>
   );
